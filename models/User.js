5cfa09adb09beef3bd71b2e6527c69060f8f83f5
gowelleast@gmail.com <gowelleast@gmail.com>
2021-04-01 00:23:34 +0900

    server

--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -31,6 +35,62 @@ const userSchema = mongoose.Schema({
     }
 })
 
+userSchema.pre('save', function (next) {
+    let user = this;
+    if (user.isModified('password')) {
+        //비밀번호를 암호화
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err);
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+})
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    //plainPassword 12345678 암호화된비밀번호 $2b$10$KoQOZOevhIcChEpYlVbfOeyvkVUl0iBGA3.Wt18HC08TWorGcg52y
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function (cb) {
+    let user = this;
+    //jsonwebtoken을 이용해서 token을 생성하기
+    let token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    user.token = token;
+    user.save(function (err, user) {
+        if (err) return cb(err)
+        cb(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function (token, cb) {
+    let user = this;
+
+    //토큰을 디코드한다.
+    jwt.verify(token, 'secretToken', function (err, decoded) {
+        //유저 아이디를 이용해서 유저를 찾은 다음에
+        //클라이언트에서 가져온 토큰과 DB에 보관된 토큰이 일치하는지 확인        
+        user.findOne({
+            "_id": decoded,
+            "token": token
+        }, function (err, user) {
+            if (err) return cb(err);
+            cb(null, user);
+        })
+    })
+}
 const User = mongoose.model('User', userSchema)
 
-module.exports = { User }
+module.exports = {
+    User
+}
